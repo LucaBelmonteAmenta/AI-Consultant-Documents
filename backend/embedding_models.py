850d9cc383523d8ee63faac1fd85d2e9b2dafabd
Lucas_Belmonte_Amenta <lucasbelmonteamenta2.0@gmail.com>
2024-10-19 11:03:45 -0300

    Ligeras mejoras del manejador de modelos de embeddings

--- backend/embedding_models.py
+++ backend/embedding_models.py
@@ -1,7 +1,10 @@
 import os
 from typing import Union, Optional, List
+
+from langchain_core.embeddings.embeddings import Embeddings
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
+
 
 
 GOOGLE_EMBEDDING_MODELS = ["models/textembedding-gecko@001",
@@ -17,6 +20,19 @@ HUGGINGFACE_EMBEDDING_MODELS = ["BAAI/bge-large-en-v1.5",
                                 "jinaai/jina-embeddings-v2-base-es"]
 
 PATH_EMBEDDING_MODELS = "./embedding_models/"
+
+
+
+def get_list_of_installed_embedding_names_models(path_models:str=PATH_EMBEDDING_MODELS) -> List[str]:
+
+  model_names = []
+
+  for item in os.listdir(path_models):
+    item_path = os.path.join(path_models, item)
+    if ((os.path.isdir(item_path)) and ("models" in item)):
+      model_names.append(item.split("models--")[-1])
+
+  return model_names
 
 
 
@@ -42,23 +58,10 @@ class EmbeddingModelsManager():
         for model_name in self.list_models_names:
             self.get_huggingface_embedding_model(model_name)
 
-  @staticmethod
-  def get_list_of_installed_embedding_names_models(path_models:str=PATH_EMBEDDING_MODELS) -> List[str]:
-
-    model_names = []
-
-    for item in os.listdir(path_models):
-      item_path = os.path.join(path_models, item)
-      if os.path.isdir(item_path):
-        model_names.append(item)
-
-    return model_names
-
-
   def get_huggingface_embedding_model(self,
                                       model_name:Optional[str] = None,
                                       multi_process: bool = False,
-                                      show_progress: bool = True) -> HuggingFaceEmbeddings:
+                                      show_progress: bool = True) -> Embeddings:
 
     model_name = self.main_model_name if model_name is None else model_name
 
@@ -81,7 +84,7 @@ class EmbeddingModelsManager():
       return self.main_model.embed_query(text)
 
 
-  def get_google_embedding_model(self, model_name: str) -> GoogleGenerativeAIEmbeddings | None:
+  def get_google_embedding_model(self, model_name: str) -> Embeddings | None:
     if "GOOGLE_API_KEY" not in os.environ:
       raise ValueError("Please set the GOOGLE_API_KEY environment variable.")
     else:
