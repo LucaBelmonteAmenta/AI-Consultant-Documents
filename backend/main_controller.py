263d269e2e24925d340140640df2143ee7a02d5f
Lucas_Belmonte_Amenta <lucasbelmonteamenta2.0@gmail.com>
2024-10-19 10:57:52 -0300

    Se cambión el nombre del controlador principal y se mejoró un poco

--- backend/main_controller.py
+++ backend/main_controller.py
@@ -1,5 +1,4 @@
-from typing import List
-
+from typing import List, Optional
 from langchain_core.documents import Document
 
 from langchain_chroma import Chroma as Chroma_langchain
@@ -14,26 +13,48 @@ from langchain.retrievers import ContextualCompressionRetriever
 from langchain.retrievers.document_compressors import FlashrankRerank
 
 from backend.document_loader import DocumentLoaderManager
-from backend.embedding_models import EmbeddingModelsManager
+from backend.embedding_models import EmbeddingModelsManager, HUGGINGFACE_EMBEDDING_MODELS
 from backend.vector_database import VectorDatabaseManager
 from backend.llm_access import LargeLanguageModelsManager
+from backend.sigleton_meta import SingletonMeta
 
 
 
-class DatabaseQuerier():
 
-  def __init__(self,
-               chroma_local:Chroma_langchain,
-               number_of_results:int=8) -> None:
+class Controller(metaclass=SingletonMeta):
 
-    self.chroma_local = chroma_local
-    self.number_of_results = number_of_results
+  init: bool = False
+
+  name_llm: str = None
+  service_token: str = None
+  temperature: str = None
+
+  name_embedding_model: str = None
+  
+  k_index: str = None
+  collection_filter : List[str] = []
+  keywords: str = None
+
+  
+  def __init__(self) -> None:
+    pass
 
 
-  def query_database(self, query:str,
-                     return_retriever:bool=True,
-                     optimized_database_query:bool=True,
-                     documents_to_filter:List[str]=[],
+
+  def register_keywords(keywords:List[str]) -> str:
+
+    list_keywords = literal_eval(keywords) if type(keywords) is str else keywords
+
+    global keywords_for_RAG
+    keywords_for_RAG = str(", ".join(list_keywords))
+
+    return keywords_for_RAG
+
+
+  def query_database(self, query: str,
+                     return_retriever: Optional[bool] = True,
+                     optimized_database_query: Optional[bool] = True,
+                     documents_to_filter: Optional[List[str]] = [],
                      ) -> List[Document] | VectorStoreRetriever | ContextualCompressionRetriever:
 
     if len(documents_to_filter) == 0:
