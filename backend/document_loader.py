02bcd1b48281e35e62e27fb7cb31e6a4268b4c96
Lucas_Belmonte_Amenta <lucasbelmonteamenta2.0@gmail.com>
2024-10-19 10:59:36 -0300

    Se actualizaron algunos componente del DocumentSplitter y DocumentLoaderManager

--- backend/document_loader.py
+++ backend/document_loader.py
@@ -1,19 +1,23 @@
+from abc import ABC
+
 from typing import Optional, List
 from langchain_core.documents import Document
 
 from re import split as re_split
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_experimental.text_splitter import SemanticChunker
+from langchain_core.embeddings.embeddings import Embeddings
 
-from langchain.document_loaders import DirectoryLoader, TextLoader
-from langchain.document_loaders.pdf import PyMuPDFLoader
-from langchain.document_loaders.xml import UnstructuredXMLLoader
-from langchain.document_loaders.html import UnstructuredHTMLLoader
-from langchain.document_loaders.csv_loader import CSVLoader
-from langchain.document_loaders.markdown import UnstructuredMarkdownLoader
-from langchain_community.document_loaders import UnstructuredWordDocumentLoader
-from langchain.document_loaders.powerpoint import UnstructuredPowerPointLoader
-from langchain.document_loaders.word_document import UnstructuredWordDocumentLoader
-from langchain.document_loaders.unstructured import UnstructuredFileLoader
+
+from langchain_community.document_loaders import DirectoryLoader, TextLoader, UnstructuredWordDocumentLoader
+from langchain_community.document_loaders.pdf import PyMuPDFLoader
+from langchain_community.document_loaders.xml import UnstructuredXMLLoader
+from langchain_community.document_loaders.html import UnstructuredHTMLLoader
+from langchain_community.document_loaders.csv_loader import CSVLoader
+from langchain_community.document_loaders.markdown import UnstructuredMarkdownLoader
+from langchain_community.document_loaders.powerpoint import UnstructuredPowerPointLoader
+from langchain_community.document_loaders.word_document import UnstructuredWordDocumentLoader
+from langchain_community.document_loaders.unstructured import UnstructuredFileLoader
 
 
 CHUNK_SIZE = 1000
@@ -49,7 +53,7 @@ LOADERS = {'.csv': CSVLoader,
 
 
 
-class DocumentLoaderManager():
+class DocumentLoaderManager(ABC):
 
   @staticmethod
   def create_directory_loader(directory_path: str, file_type : str = '.pdf') -> DirectoryLoader:
@@ -97,9 +101,12 @@ class DocumentLoaderManager():
 
 
 
+
+
 class DocumentSplitter():
 
-  def __init__(self, separators:List[str] = SEPARATORS_DEFAULTS) -> None:
+  def __init__(self, separators:List[str] = SEPARATORS_DEFAULTS, embeddings:Optional[Embeddings]=None) -> None:
+
     self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=CHUNK_SIZE,
                                                         chunk_overlap=CHUNK_OVERLAP,
                                                         separators=separators,
@@ -107,6 +114,32 @@ class DocumentSplitter():
                                                         add_start_index=True,
                                                         length_function=len,
                                                         is_separator_regex=False)
+    if (embeddings is not None):
+      self.experimental_text_splitter = SemanticChunker(embeddings = embeddings,
+                                                        add_start_index = False,
+                                                        breakpoint_threshold_type = "percentile",
+                                                        min_chunk_size = 90,
+                                                        number_of_chunks = None,
+                                                        sentence_split_regex = r"(?<=[.?!])\s+" + "\n")
+    else:
+      self.experimental_text_splitter = None
+
+
+  def experimental_split_documents(self, docs : List[Document], source_name: str|None = None) -> List[Document] | None:
+
+    if self.experimental_text_splitter is not None:
+
+      splits = self.experimental_text_splitter.split_documents(docs)
+
+      for idx, text in enumerate(splits):
+        if source_name is not None:
+          splits[idx].metadata['source'] = source_name
+        else:
+          splits[idx].metadata['source'] = re_split(r"\.|/", splits[idx].metadata['source'])[-2]
+
+      return splits
+    else:
+      return None
 
 
   def split_documents(self, docs : List[Document], source_name: str|None = None) -> List[Document]:
@@ -118,5 +151,6 @@ class DocumentSplitter():
         splits[idx].metadata['source'] = source_name
       else:
         splits[idx].metadata['source'] = re_split(r"\.|/", splits[idx].metadata['source'])[-2]
+    print(splits)
 
     return splits
