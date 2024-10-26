2a03b1136e7cb6b1a8d19edfb9a323b3b51410d0
Lucas_Belmonte_Amenta <lucasbelmonteamenta2.0@gmail.com>
2024-10-26 20:10:19 -0300

    se retiró el modificador de k_index de la interfaz de configuración
    se reubicará el configurador de k_index en la interfaz de chat, cuando se decida completar el expander_advanced_options.

--- frontend/configuration.py
+++ frontend/configuration.py
@@ -1,17 +1,7 @@
-import numpy as np
-import pandas as pd
 import streamlit as st
 from backend.embedding_models import get_list_of_installed_embedding_names_models
 from backend.llm_access import LLM_GOOGLE, LLM_OLLAMA, LLM_OPENAI, PAID_SERVICE
 from backend.consultant_controller import ConsultantController as Controller
-
-
-
-k_explanation_text = """
-K: número de fragmentos de documento de la base de datos que es extraído por el LLM por cada consulta, como fuente de información para responder a la misma.\n
-Mientas más fragmentos de documentos emplee el modelo, mas tokens consumirá la respectiva consulta realizada. Tome esto en cuenta si emplea un servicio pago para el LLM, o si el mismo posee un límite máximo de tokens por consulta.\n
-Tome en cuenta que, a mayor K también aumenta la probabilidad de que uno de los fragmentos extraídos posea la información necesaria para responder a la consulta realizada, pero a la vez aumenta la probabilidad de alucinaciones en la respuesta por parte del LLM.
-"""
 
 
 
@@ -21,7 +11,7 @@ def ConfigurationUI():
     
     container_embedding = st.container(border=True)
     container_llm = st.container(border=True)
-    container_query = st.container(border=True)
+    container_database = st.container(border=True)
 
     list_embedding_models = get_list_of_installed_embedding_names_models()
     lists_llm = {"Gemini Developer API":LLM_GOOGLE, "Ollama":LLM_OLLAMA, "OpenAI API":LLM_OPENAI}
@@ -41,10 +31,6 @@ def ConfigurationUI():
                                                         disabled=(controller.llm_origin not in PAID_SERVICE))
 
     
-    controller.k_index = container_query.number_input(label="Introduzco el indice K: ", 
-                                                      max_value=10, min_value=1, value=3)
-    container_query.container(border=True).write(k_explanation_text)
-
 
 
 
