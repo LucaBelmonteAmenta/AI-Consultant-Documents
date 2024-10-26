a4898b0348d7dcc169770db0cff02f1f0c502f2c
Lucas_Belmonte_Amenta <lucasbelmonteamenta2.0@gmail.com>
2024-10-26 20:16:55 -0300

    Update consultant_controller.py
    Se realizaron múltiples cambios para poder llevar a cabo los cambios de los últimos commits realizados

--- backend/consultant_controller.py
+++ backend/consultant_controller.py
@@ -1,3 +1,4 @@
+from config import APP_PATH
 from time import sleep
 from ast import literal_eval
 from typing import List, Optional, Literal, Dict
@@ -15,6 +16,7 @@ from langchain.agents import Tool, create_react_agent, AgentExecutor
 from langchain_core.vectorstores import VectorStoreRetriever
 from langchain.retrievers import ContextualCompressionRetriever
 from langchain_community.document_compressors.flashrank_rerank import FlashrankRerank
+from flashrank import Ranker
 
 
 from backend.system_prompts import (system_prompt_LLM_keywords, 
@@ -43,18 +45,26 @@ class ConsultantController(metaclass=SingletonMeta):
   
   k_index: str = 3
   collection_filter : List[str] = []
+  enter_keywords_manually: bool = False
   keywords: str = None
 
+  optimized_database_query_enabled: bool = False
+  multi_process_embedding : bool = False
+
+  embedding_manager:EmbeddingModelsManager = None
+  database_manager:VectorDatabaseManager = None
+  
   
   def __init__(self) -> None:
-    self.embedding = None
-    self.database_manager = None
+    self.init = False
+    self.embedding_model = None
 
 
   def prepare_system(self) -> None:
 
-    self.embedding = EmbeddingModelsManager(self.name_embedding_model, False).get_huggingface_embedding_model()
-    self.database_manager = VectorDatabaseManager(self.embedding)
+    self.embedding_model = self.embedding_manager.get_huggingface_embedding_model(model_name=self.name_embedding_model,
+                                                                                  multi_process = self.multi_process_embedding)
+    self.database_manager = VectorDatabaseManager(self.embedding_model)
 
     match self.llm_origin:
       case "Gemini Developer API":
@@ -110,24 +120,32 @@ class ConsultantController(metaclass=SingletonMeta):
   def is_system_configured(self) -> bool:
     llm_conditions_1 = (self.llm_origin in PAID_SERVICE) and (self.service_token is not None) 
     llm_conditions_2 = (self.temperature is not None) and (self.name_llm is not None)
-    embedding_conditions = (self.name_embedding_model is not None) and (self.embedding is not None)
-    #database_conditions = (self.database_manager is not None) and (len(self.database_manager.get_list_collections()) > 0)
-    required_parameters = [self.init, llm_conditions_1, llm_conditions_2, embedding_conditions]
-    return required_parameters
+    embedding_conditions = (self.embedding_manager is not None) and (self.name_embedding_model is not None) 
+    database_conditions = (self.database_manager is not None) and (len(self.database_manager.get_list_collections()) > 0)
+    required_parameters = [self.init, llm_conditions_1, llm_conditions_2, embedding_conditions, database_conditions]
+    return all(required_parameters)
   
   
   def answer_query(self, query:str) -> Dict | None:
     if self.is_system_configured():
+      
       self.prepare_system()
-      keywords = self.get_keywords(query=query, iterations=1, max_execution_time=10000)
-      possible_error_messag = "Agent stopped due to iteration limit or time limit."
-      if ((keywords == possible_error_messag) and (keywords_for_RAG is not None)):
-        keywords = keywords_for_RAG
-      context_docs = self.query_database(query=keywords, return_retriever=False, optimized_database_query=False)
+
+      if (not ((self.enter_keywords_manually) and (self.keywords is not None))):
+        self.keywords = self.get_keywords(query=query, iterations=1, max_execution_time=1000)
+        possible_error_messag = "Agent stopped due to iteration limit or time limit."
+        if ((self.keywords == possible_error_messag) and (keywords_for_RAG is not None)):
+          self.keywords = keywords_for_RAG
+
+      context_docs = self.query_database(query=self.keywords, 
+                                         return_retriever=False, 
+                                         optimized_database_query=self.optimized_database_query_enabled)
+      
       prompt = self.create_prompt(system_prompt_query_responder)
       chain = create_stuff_documents_chain(self.llm, prompt)
       answer = chain.invoke({'input' : query, "context": context_docs})
-      return {"output": answer ,"context_docs": context_docs, "keywords": keywords}
+  
+      return {"output": answer ,"context_docs": context_docs, "keywords": self.keywords}
     else:
       return None
 
@@ -137,7 +155,9 @@ class ConsultantController(metaclass=SingletonMeta):
                      optimized_database_query: Optional[bool] = True,
                      documents_to_filter: Optional[List[str]] = [],
                      ) -> List[Document] | VectorStoreRetriever | ContextualCompressionRetriever:
+    
     database = self.database_manager.get_or_create_database()
+
     if len(documents_to_filter) == 0:
       retriever = database.as_retriever(search_kwargs= { "k": self.k_index } )
     else:
@@ -167,13 +187,17 @@ class ConsultantController(metaclass=SingletonMeta):
 
 
   def get_compression_retriever(self, retriever:VectorStoreRetriever) -> ContextualCompressionRetriever:
-    compressor = FlashrankRerank(top_n=self.k_index)
+    model_Multilingual_name = "ms-marco-MultiBERT-L-12"
+    model_path = APP_PATH + "/rerank_models/"
+    flashrank_client = Ranker(model_name=model_Multilingual_name, cache_dir=model_path)
+    compressor = FlashrankRerank(client=flashrank_client, top_n=self.k_index, model=model_Multilingual_name)
     compression_retriever = ContextualCompressionRetriever(base_compressor=compressor,
                                                            base_retriever=retriever)
     return compression_retriever
 
 
-  def query_with_LLM(self, llm,
+  def query_with_LLM(self, 
+                     llm,
                      query:str,
                      system_prompt_LLM:str,
                      documents_to_filter:List[str]=[]):
